d3d40140a3a6d2ca6e4ed711df9cb4b9e365f010
Hajer Doghmen <hajerdoghmen123@outlook.fr>
2021-03-14 20:32:55 +0100

    AjoueDiscount

---
+++ Domain/Discount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PFE.Domain
+{
+    public class Discount
+    {
+        public int DiscoutId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Percent { get; set; }
+
+    }
+}
---
+++ Repository/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using PFE.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PFE.Repository
+{
+    public interface IDiscountRepository
+    {
+       List< Discount> GetDiscountById(int articleId);
+    }
+}
